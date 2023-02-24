9e959161d8d6a519eb24b7af14f440e34b935249
Geoff Allen <zengadev@gmail.com>
2023-02-24 12:45:52 -0600

    update paths

--- src/main/java/com/redhat/custommetricsocp/DemoApplication.java
+++ src/main/java/com/redhat/custommetricsocp/DemoApplication.java
@@ -1,13 +1,24 @@
 package com.redhat.custommetricsocp;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
+	@RequestMapping("/hello")
+	@ResponseBody
+    String home() {
+        return "Hello oc World!";
+    }
 
 }
